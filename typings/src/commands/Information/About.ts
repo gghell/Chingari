51cc230da61cc942cade8556fbf1b3193f6b4e5c
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-06-28 14:28:35 +0600

    Update About.ts

--- typings/src/commands/Information/About.ts
+++ typings/src/commands/Information/About.ts
@@ -1,1 +1,47 @@
-mm
+import AvonCommand from "../../base/AvonCommand.js";
+
+import { Message } from "discord.js";  // Assuming you're using discord.js
+
+export default class About extends AvonCommand {
+
+  name: string;
+
+  desc: string;
+
+  usage: string;
+
+  aliases: string[];
+
+  cat: string;
+
+  constructor(client: any) {  // You might want to replace 'any' with a more specific type if available
+
+    super(client);
+
+    this.name = "about";
+
+    this.desc = "Provides you with the information of the bot";
+
+    this.usage = "about";
+
+    this.aliases = ["botinfo", "bi"];
+
+    this.cat = "info";
+
+  }
+
+  exec = async (message: Message, args: string[], prefix: string): Promise<void> => {
+
+    return message.reply({
+
+      embeds: [
+
+        this.client.utils.premiumEmbed(message.guildId).setDescription("Information about the bot."),
+
+      ],
+
+    });
+
+  };
+
+}
